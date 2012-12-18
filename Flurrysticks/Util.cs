1da35ee2e059d5be3a6c52b878d6c719bd08bde1
ursimon <michal.ursiny@gmail.com>
2012-12-18 18:40:33 +0100

    flurrysticks w8 wip switching metrics
    Signed-off-by: ursimon <michal.ursiny@gmail.com>

---
+++ Flurrysticks/Util.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Flurrysticks
+{
+    class Util
+    {
+
+        public static T FindFirstElementInVisualTree<T>(DependencyObject parentElement) where T : DependencyObject
+        {
+            var count = VisualTreeHelper.GetChildrenCount(parentElement);
+            if (count == 0)
+                return null;
+
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parentElement, i);
+
+                if (child != null && child is T)
+                {
+                    return (T)child;
+                }
+                else
+                {
+                    var result = FindFirstElementInVisualTree<T>(child);
+                    if (result != null)
+                        return result;
+
+                }
+            }
+            return null;
+        }
+
+    }
+}
